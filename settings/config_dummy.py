9c7d1a78b1ecba6d94921f09392c410f718193f8
paulchertov <paulchertov@gmail.com>
2017-07-26 20:15:39 +0300

    Initial commit

---
+++ settings/config_dummy.py
@@ -0,0 +1,6 @@
+﻿# Yandex Direct API token
+YA_DIRECT_TOKEN = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+# YA_DIRECT_URL = ""
+# Yandex sandbox url
+YA_DIRECT_URL = "https://api-sandbox.direct.yandex.com/json/v5/"
