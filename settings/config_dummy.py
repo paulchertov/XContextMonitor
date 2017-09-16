0efea091fd64506d1f8e6ee3b9c0bba91773da8b
paulchertov <paulchertov@gmail.com>
2017-09-16 11:04:23 +0300

    rewrited config

--- settings/config_dummy.py
+++ settings/config_dummy.py
@@ -1,6 +1,3 @@
-﻿# Yandex Direct API token
-YA_DIRECT_TOKEN = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-
-# YA_DIRECT_URL = ""
-# Yandex sandbox url
-YA_DIRECT_URL = "https://api-sandbox.direct.yandex.com/json/v5/"
+﻿YA_DIRECT_TOKEN = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+YA_DIRECT_URL = "https://api.direct.yandex.com/json/v5/"
+# YA_DIRECT_URL = "https://api-sandbox.direct.yandex.com/json/v5/"
