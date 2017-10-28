77931660c92ddb007ad43797c7c39e01448ad1cb
paulchertov <paulchertov@gmail.com>
2017-10-28 14:53:25 +0300

    Added clent tasks

--- tasks/db/client.py
+++ tasks/db/client.py
@@ -1,28 +1,82 @@
-from PyQt5.QtCore import pyqtSignal, pyqtSlot
+"""
+Module with DB tasks for working with clients table
+in db
+Classes:
+
+"""
+
+from typing import List
+
+from PyQt5.QtCore import pyqtSignal
 
 from model.alchemy.client import YandexClient
 from model.api_items.yandex import YaAPIDirectClient
 from tasks.db.common import PQDBTask
 
 
-class LoadClients(PQDBTask):
-    got_clients = pyqtSignal(list)
+def all_clients(session)->List[YaAPIDirectClient]:
+    """
+    Internal function for getting all clients from db
+    :param session: db session
+    :return: list of all clients in db
+    """
+    return [
+        YaAPIDirectClient(
+            login=client.login,
+            token=client.token,
+            timestamp=client.timestamp,
+            is_active=client.set_active
+        )
+        for client in session.query(YandexClient).all()
+    ]
 
-    def __init__(self):
-        super().__init__()
+
+class LoadClients(PQDBTask):
+    """
+    DB Task that updates db with all clients, saved to json
+    and then gets all clients from db
+    """
+    got_clients = pyqtSignal(list)
 
     def run(self):
         try:
             YandexClient.load_json(self.session)
-            self.got_clients.emit([
-                YaAPIDirectClient(
-                    login=client.login,
-                    token=client.token,
-                    timestamp=client.timestamp,
-                    is_active=client.set_active
-                )
-                for client in self.session.query(YandexClient).all()
-            ])
+            self.got_clients.emit(all_clients(self.session))
         except Exception as e:
             self.error_occurred.emit(e)
 
+
+class GetClients(PQDBTask):
+    """
+    DB task that gets all clients from DB 
+    and then gets all clients from db
+    """
+    got_clients = pyqtSignal(list)
+
+    def run(self):
+        try:
+            self.got_clients.emit(all_clients(self.session))
+        except Exception as e:
+            self.error_occurred.emit(e)
+
+
+class AddClient(PQDBTask):
+    """
+    DB task that adds one client
+    and then gets all clients from db
+    """
+    got_clients = pyqtSignal(list)
+
+    def __init__(self, client: YaAPIDirectClient):
+        """
+        :param client: client that will be added to DB
+        """
+        super().__init__()
+        self.client = client
+
+    def run(self):
+        try:
+            YandexClient.add_single(self.session, self.client)
+            self.got_clients.emit(all_clients(self.session))
+        except Exception as e:
+            self.error_occurred.emit(e)
