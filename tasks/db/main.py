606561393c38c32e5a88043b305e71b09787b9e7
paulchertov <paulchertov@gmail.com>
2017-12-07 15:36:25 +0300

    Added db config task

---
+++ tasks/db/main.py
@@ -0,0 +1,28 @@
+"""
+Module containing all tasks that manage DB state
+classes:
+    InitDB - init task for db
+"""
+
+from PyQt5.QtCore import QThread, pyqtSignal
+
+from model.alchemy.common import Base, engine
+from model.alchemy.campaign import YandexCampaign
+from model.alchemy.ad_group import YandexAdGroup
+from model.alchemy.ad import YandexAd
+from model.alchemy.links import YandexLink, YandexLinksSet, LinkUrl
+
+
+
+class InitDB(QThread):
+    """
+    Task that initialises database
+    """
+    error_occurred = pyqtSignal(Exception)
+
+    def run(self):
+        try:
+            Base.metadata.create_all(engine)
+            self.finished.emit()  # does not emits don't know why
+        except Exception as e:
+            self.error_occurred.emit(e)
